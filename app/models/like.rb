342d2d1caab7ad09f3886f4c32739bcd9c9b88fb
Daniel Michael <itzdanielmichael@gmail.com>
2021-01-08 19:06:44 -0500

    update like.rb and insights_controller.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :insight
 
+  # a insight_id should be unique for the user_id
+  validates :insight, uniqueness: {scope: :user}
   def insight_name
     self.insight.title
   end
---
+++ db/migrate/20210109000634_add_index_to_likes.rb
@@ -0,0 +1,4 @@
+class AddIndexToLikes < ActiveRecord::Migration[6.0]
+  def change
+  end
+end

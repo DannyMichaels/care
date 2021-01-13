c26c71b0864e7385a9b762b06ae966dcac9311f4
Daniel Michael <itzdanielmichael@gmail.com>
2021-01-13 10:59:38 -0500

    update like.rb, likes_controller.rb, create add_index_to_likes.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,9 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :insight
 
+    # a insight_id should be unique for the user_id
+    validates :insight, uniqueness: {scope: :user}
+    
   def insight_name
     self.insight.title
   end
